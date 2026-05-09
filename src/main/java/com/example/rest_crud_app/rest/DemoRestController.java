59b48252374c14ed8d5445ed4f7a3b6fb7e4cbba
Cristian Cretu <cretu.3210@gmail.com>
2026-05-09 19:18:51 +0300

    Initial commit

---
+++ src/main/java/com/example/rest_crud_app/rest/DemoRestController.java
@@ -0,0 +1,19 @@
+package com.example.rest_crud_app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+
+public class DemoRestController {
+
+    //adaugam codul pentru endpont-ul / hello
+        @GetMapping("/hello")
+        public String sayHello(){
+            return "Hello World";
+        }
+    }
+
+
